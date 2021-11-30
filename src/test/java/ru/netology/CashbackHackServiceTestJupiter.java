e35fe42b04c4f6b7d188581696dfbad622575705
Olga Stashenyuk <olgastachenyk@mail.ru>
2021-11-30 13:56:59 +0300

    junit4-platform

---
+++ src/test/java/ru/netology/CashbackHackServiceTestJupiter.java
@@ -0,0 +1,43 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class CashbackHackServiceTestJupiter {
+
+    @Test
+    public void shouldTestRemainFrom0() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 0;
+
+        int actual = service.remain(payment);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void shouldTestRemainFrom1() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 1;
+
+        int actual = service.remain(payment);
+        int expected = 999;
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void shouldTestRemainFrom1000() {
+        CashbackHackService service = new CashbackHackService();
+        int payment = 1000;
+
+        int actual = service.remain(payment);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+}
